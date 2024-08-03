1429af03e73ba342ac2638fa65b3c7beda59bc83
Filiphe <filiphenatannael@gmail.com>
2024-08-03 18:41:23 -0300

    Autenticacao Funcionando

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,15 +1,20 @@
 //Arquivo que irá conter cada rota de user
+// src/routes/userRoutes.js
 import express from 'express';
-import UserController from "../controllers/userController.js";
-import { body } from 'express-validator';
+import UserController from '../controllers/userController.js';
+import authMiddleware from '../middleware/authMiddleware.js';
 
 const routes = express.Router();
 
+// Rota pública para registro e login
 routes.post('/register', UserController.userRegister);
-routes.post('/login', UserController.userLogin); 
-routes.get('/users', UserController.findUser);
-routes.get('/users/:id', UserController.findUserById);
-routes.put('/users/:id', UserController.userUpdate);
-routes.delete('/users/:id', UserController.userDelete);
+routes.post('/login', UserController.userLogin);
+
+// Rota privada - precisa de autenticação
+routes.get('/users', authMiddleware, UserController.findUser);
+routes.get('/users/:id', authMiddleware, UserController.findUserById);
+routes.put('/users/:id', authMiddleware, UserController.userUpdate);
+routes.delete('/users/:id', authMiddleware, UserController.userDelete);
 
 export default routes;
+
